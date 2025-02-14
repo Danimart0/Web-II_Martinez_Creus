ffc98ffece339275ae218b7f011287100099d5f9
DanielMartinez <danielmartinezcreus@gmail.com>
2025-02-13 18:25:31 -0600

    Mostrar Id
    Se hizo un cambio en models.py para que pueda mostrar el id en la carta

--- app-modelo-clase/django-app-modelo/modelo_app/users/models.py
+++ app-modelo-clase/django-app-modelo/modelo_app/users/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
-    profile_image = models.URLField()  
+    profile_image = models.URLField()
 
     def __str__(self):
-        return self.name
+      
+        return f"ID: {self.id} - Name: {self.name} - Age: {self.age}"
