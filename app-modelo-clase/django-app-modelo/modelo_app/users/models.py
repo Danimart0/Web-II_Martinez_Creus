e80f7dbe368e0d6507c42abff9fb0f6160ca7b3c
DanielMartinez <danielmartinezcreus@gmail.com>
2025-02-17 17:05:14 -0600

    correcion final
    users

--- app-modelo-clase/django-app-modelo/modelo_app/users/models.py
+++ app-modelo-clase/django-app-modelo/modelo_app/users/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True, default="default@example.com")
     age = models.IntegerField()
-    profile_image = models.URLField()
+    rfc = models.CharField(max_length=13, default="RFC_DEFAULT")
+    photo = models.URLField(blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-      
-        return f"ID: {self.id} - Name: {self.name} - Age: {self.age}"
+    def _str_(self):
+        return self.name
+
+class UserAddress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="addresses")
+    street = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return f"{self.street}, {self.city}, {self.country}"
