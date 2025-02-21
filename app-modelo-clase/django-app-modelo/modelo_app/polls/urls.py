7633d5eede955028abb27c85fd2efc9c205f56ca
DanielMartinez <danielmartinezcreus@gmail.com>
2025-02-21 16:37:57 -0600

    cambios en css
    Se cambio el css

--- app-modelo-clase/django-app-modelo/modelo_app/polls/urls.py
+++ app-modelo-clase/django-app-modelo/modelo_app/polls/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),         # Ruta para la vista index
+    path("polls/", views.index, name="index"),         # Ruta para la vista index
     path("orders/", views.indexOrders, name="indexOrders"), 
 ]
